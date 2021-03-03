4a64b8e09b6d83d3ab51696dbd0624aef8e0ff79
John D <jdealla@gmail.com>
2021-03-03 09:28:26 -0500

    added averages build

--- src/templates/averages.js
+++ src/templates/averages.js
@@ -6,18 +6,40 @@ const averages = data => {
     const efgp = ( (fgm - fg3m) + 1.5 * fg3m) / fga;
     const tsp = pts / ( 2 * (fga + 0.44 * fta));
     const tovp = 100 * turnover / (fga + 0.44 * fta + turnover);
-    const round = num => Math.round(num * 100).toFixed(2);
-    let response = "";
+    const round = num => Math.round(num * 100).toFixed(2);   
+    
+    // let response = "";
+    // response += "# ".concat(first_name, " ").concat(last_name, "\n");
+    // response += "### ".concat(season, " - ").concat(full_name, "\n");
+    // response += "| GP | PPG | RPB | APG | BPG    | SPG    | FPG   |  TPG        |\n";
+    // response += "| --- | --- | --- | --- | --- | --- | ---| --- |\n";
+    // response += "| ".concat(games_played, " | ").concat(pts, " | ").concat(reb, "|  ").concat(ast, "| ").concat(blk, " |  ").concat(stl, "| ").concat(pf, " | ").concat(turnover, " |\n\n\n\n");
+    // response += "| MPG | FG% | FT% | 2P% | 3P% | eFG% | TS% | TOV% |\n";
+    // response += "| --- | --- | --- | --- | --- | ---  | --- | ---  |\n";
+    // response += "| ".concat(min, "| ").concat(round(fg_pct), " | ").concat(round(ft_pct), " | ").concat(twoPper.toFixed(2), " | ").concat(round(fg3_pct * 100), " |  ").concat(round(efgp), " | ").concat(round(tsp), " | ").concat(tovp.toFixed(2), " |\n");
+    // response += "";
+    // let content = "<div class='row'>";
+    // content += "</div>";
 
-    response += "# ".concat(first_name, " ").concat(last_name, "\n");
-    response += "### ".concat(season, " - ").concat(full_name, "\n");
-    response += "| GP              | PPG    | RPB   |   APG  | BPG    | SPG    | FPG   |  TPG        |\n";
-    response += "| --------------- | ----   | ----  | ---    |-----   | -----  | ----- | -----       |\n";
-    response += "| ".concat(games_played, " | ").concat(pts, " | ").concat(reb, "|  ").concat(ast, "| ").concat(blk, " |  ").concat(stl, "| ").concat(pf, " | ").concat(turnover, " |\n\n\n\n");
-    response += "| MPG   | FG%                    | FT%                    | 2P%               | 3P%                     | eFG%            | TS%           | TOV% |\n";
-    response += "| ---   | ----                   | ----                   | ----              | -----                   | -----           | -----         |----- |\n";
-    response += "| ".concat(min, "| ").concat(round(fg_pct), " | ").concat(round(ft_pct), " | ").concat(round(twoPper), " | ").concat(round(fg3_pct * 100), " |  ").concat(round(efgp), " | ").concat(round(tsp), " | ").concat(round(tovp), " |\n");
-    response += "";
+    let response = "";
+    response += "## **" + first_name + " " + last_name + "**\n";
+    response += "#### **" + season + " - " + full_name + "**\n";
+    response +=  "***\n";
+    response += "| GP  | PPG | RPB | APG |\n";
+    response += "| --- | --- | --- | --- |\n";
+    response += "| ".concat(games_played, " | ").concat(pts, " | ").concat(reb, "|  ").concat(ast, "|\n\n\n\n")
+
+    response += "| BPG | SPG | FPG | TPG |\n";
+    response += "| --- | --- | --- | --- |\n";
+    response += "| ".concat(blk, " |  ").concat(stl, "| ").concat(pf, " | ").concat(turnover, " |\n\n\n\n");
+
+    response += "| MPG | FG% | FT% | 2P% |\n";
+    response += "| --- | --- | --- | --- |\n";
+    response += "| ".concat(min, "| ").concat(round(fg_pct), " | ").concat(round(ft_pct), " | ").concat(twoPper.toFixed(2), " |\n\n\n\n");
+
+    response += "| 3P% | eFG% | TS% | TOV% |\n";
+    response += "| --- | --- | --- | --- |\n";
+    response += "| ".concat(round(fg3_pct), " |  ").concat(round(efgp), " | ").concat(round(tsp), " | ").concat(tovp.toFixed(2), " |\n");
 
     return response;
 };
